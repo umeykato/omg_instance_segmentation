d67ab1bbe5e027670c979d1e6f3195afbe628674
DESKTOP-RMFRGL4\y_kato <y_kato@umelab.jp>
2018-11-19 11:55:03 +0900

    start

---
+++ src/oomugi/dataset.py
@@ -0,0 +1,177 @@
+import numpy as np
+import cv2
+import glob
+import os
+
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+
+class SSSDataset(Dataset):
+    def __init__(self, train, n_sticks=8, data_size=3535):
+        super().__init__()
+        self.train = train
+        self.n_sticks = n_sticks
+        self.data_size = data_size
+        self.height = 256
+        self.width = 256
+
+        # #ver1.0
+        # self.img_path = 'I:/ykato_git/datasets/oomugi_blender/train/o/20/'
+        # self.sem_path = 'I:/ykato_git/datasets/oomugi_blender/train/f/20/'
+        # self.ins_path = 'I:/ykato_git/datasets/oomugi_blender/train/s/20/'
+
+        #ver2.0
+        # self.img_path = 'I:/ykato_git/datasets/oomugi_blender/train2/image/'
+        # self.sem_path = 'I:/ykato_git/datasets/oomugi_blender/train2/semantic/'
+        # self.ins_path = 'I:/ykato_git/datasets/oomugi_blender/train2/instance/'
+
+        # ver3.0
+        root = 'I:/ykato_git/datasets/oomugi_blender/dataset_ver3/dataset_SemInsSpline'
+        self.img_path = root + '/image/'
+        self.sem_path = root + '/semantic/'
+        self.ins_path = root + '/instance_segment/'
+        self.spline_path = root + '/spline/'
+
+        self.png_name = os.listdir(self.img_path)
+
+    def __len__(self):
+        return self.data_size
+
+    '''
+    img (ch=1, height, width)
+        白背景に全スティック領域の黒枠（前後関係を考慮済み）
+    sem (ch=1, height, width)
+        背景0 スティック領域1
+    ins (ch=スティックの本数, height, width)
+        背景0 スティック領域1，スティック本数を８に固定
+
+    変更後
+    img オオムギ画像 3ch
+    sem オオムギ領域画像 1ch
+    ins 大麦オブジェクト画像，オブジェクト数30～60 = 30~60ch
+    '''
+
+    def __getitem__(self, index):
+
+        # カラー画像と同サイズのarray生成
+        img = np.ones((3, self.height, self.width), dtype=np.uint8)
+        # カラー画像読み込み
+        img_temp = cv2.imread(self.img_path + self.png_name[index])
+        # カラー画像リサイズ
+        img_temp = cv2.resize(img_temp, (self.height, self.width), interpolation= cv2.INTER_NEAREST)
+        # デバッグ保存
+        # cv2.imwrite('./img_temp.png', img_temp)
+        # カラー画像トランスポーズ
+        img = img_temp.transpose(2, 0, 1)
+
+        # instance画像と同サイズのarray生成（ｃｈ数＝最大オブジェクト数）
+        ins = np.zeros((60, self.height, self.width), dtype=np.uint8)
+        # instance画像のパス生成
+        ins_dir = self.ins_path + self.png_name[index].rstrip('.png') + '/'
+        # オブジェクト数を格納
+        ins_num = len(os.listdir(ins_dir))
+        # 現在のオブジェクト数初期化
+        object_num = 0
+        # 白色画像生成
+        ins_one = np.ones((self.height, self.width), dtype=np.uint8) * 255
+
+        # instance画像をnumpyに格納
+        # for i in range(0, ins_num-1):
+        for i in range(1, ins_num):
+            # instance画像生成
+            ins_temp = cv2.imread(ins_dir + '{}.png'.format(i), 0)
+            # デバッグ用
+            # cv2.imwrite('./ins_{}_org.png'.format(i), ins_temp)
+            # instance画像リサイズ
+            ins_temp = cv2.resize(ins_temp, (self.height, self.width), interpolation= cv2.INTER_NEAREST)
+            # リサイズした画像にラベルとなる部分が残っていれば
+            if(np.sum(ins_temp == ins_one) != 0):
+                # デバッグ用
+                # cv2.imwrite('./ins_{}.png'.format(object_num), ins_temp)
+                # 255で割って正規化したやつを（現在のオブジェクト数）chとして代入
+                ins[object_num] = ins_temp / 255
+                # 現在のオブジェクト数をインクリメント
+                object_num += 1
+
+
+        # semantic画像と同サイズarrayを生成
+        sem = np.zeros((self.height, self.width), dtype=bool)
+        # instanceで塗った所をラベル（True）にする
+        sem[np.sum(ins, axis=0) != 0] = True
+        # デバッグ用
+        # cv2.imwrite('./sem_temp.png', sem.astype(np.uint8)*255)
+        # semantic画像と反転した画像(2ch)の配列にする
+        sem = np.stack([~sem, sem]).astype(np.uint8)
+
+        # 3 * height * width
+        img = torch.Tensor(img.astype(np.uint8))
+        # 2 * height * width
+        sem = torch.Tensor(sem)
+        # 60 * height * width
+        ins = torch.Tensor(ins)    
+
+        return img, sem, ins, object_num
+
+
+
+
+        ###############################################################
+
+        # while True:
+        #     img = np.ones((self.height, self.width), dtype=np.uint8) * 255
+        #     ins = np.zeros((0, self.height, self.width), dtype=np.uint8)
+        #     for _ in range(self.n_sticks):
+        #         x = np.random.randint(30, 225)
+        #         y = np.random.randint(30, 225)
+        #         w = 15
+        #         h = np.random.randint(80, 100)
+        #         theta = np.random.randint(-90, 90)
+        #         rect = ([x, y], [w, h], theta)
+        #         box = np.int0(cv2.boxPoints(rect))
+
+        #         gt = np.zeros_like(img)
+        #         gt = cv2.fillPoly(gt, [box], 1)
+        #         ins[:, gt != 0] = 0
+        #         ins = np.concatenate([ins, gt[np.newaxis]])
+        #         img = cv2.fillPoly(img, [box], 255)
+        #         img = cv2.drawContours(img, [box], 0, 0, 2)
+
+        #     # minimum area of stick
+        #     if np.sum(np.sum(ins, axis=(1, 2)) < 400) == 0:
+        #         break
+
+        # if self.train:
+        #     sem = np.zeros_like(img, dtype=bool)
+        #     sem[np.sum(ins, axis=0) != 0] = True
+        #     sem = np.stack([~sem, sem]).astype(np.uint8)
+
+        #     print(img[np.newaxis].shape)
+        #     print(sem.shape)
+        #     print(ins.shape)
+
+        #     # 1 * height * width
+        #     img = torch.Tensor(img[np.newaxis])
+        #     # 2 * height * width
+        #     sem = torch.Tensor(sem)
+        #     # n_sticks * height * width
+        #     ins = torch.Tensor(ins)
+        #     return img, sem, ins
+        # else:
+        #     # 1 * height * width
+        #     img = torch.Tensor(img[np.newaxis])
+        #     return img
+
+
+if __name__=='__main__':
+    dataset = SSSDataset(train=True, n_sticks=8)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=False, num_workers=0, pin_memory=True)
+
+    img, sem, ins = dataloader
+
+    # for epoch in range(10):
+    #     for batched in dataloader:
+    #         img, sem, ins = batched
+
+
---
+++ src/oomugi_add_spline/model.py
@@ -0,0 +1,166 @@
+"""
+This implementation is based on following code:
+https://github.com/milesial/Pytorch-UNet
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+
+
+class double_conv(nn.Module):
+    '''(conv => BN => ReLU) * 2'''
+    def __init__(self, in_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class inconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(inconv, self).__init__()
+        self.conv = double_conv(in_ch, out_ch)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            double_conv(in_ch, out_ch)
+        )
+
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+
+class up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True):
+        super(up, self).__init__()
+
+        #  would be a nice idea if the upsampling could be learned too,
+        #  but my machine do not have enough memory to handle all those weights
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear')
+        else:
+            self.up = nn.ConvTranspose2d(in_ch, out_ch, 2, stride=2)
+
+        self.conv = double_conv(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffX = x1.size()[2] - x2.size()[2]
+        diffY = x1.size()[3] - x2.size()[3]
+        x2 = F.pad(x2, (diffX // 2, int(diffX / 2),
+                        diffY // 2, int(diffY / 2)))
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class outconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(outconv, self).__init__()
+        # self.conv = nn.Conv2d(in_ch, out_ch, 1)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, in_ch//2, 1),
+            nn.BatchNorm2d(in_ch//2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_ch//2, out_ch, 1),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class outlinear(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(outlinear, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, 1, 1),
+            nn.BatchNorm2d(1),
+            nn.ReLU(inplace=True),
+        )
+        self.linear = nn.Sequential(
+            nn.Linear(256*256, 4096),
+            nn.ReLU(inplace=True),
+            # nn.Linear(4096, 4096),
+            # nn.ReLU(inplace=True),
+            nn.Linear(4096, out_ch),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        # print(x.size())
+        x = x.view(x.size(0), -1)
+        # print(x.size())
+        x = self.linear(x)
+        return x
+
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+        # self.inc = inconv(1, 64)
+        self.inc = inconv(3, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up(1024, 256)
+        self.up2 = up(512, 128)
+        self.up3 = up(256, 64)
+        self.up4 = up(128, 64)
+        self.sem_out = outconv(64, 2)
+        # self.ins_out = outconv(64, 16)
+        self.ins_out = outconv(64, 60)
+        self.spline_out = outlinear(64, 60 * 8 * 2)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        # print('x1 : ', x1.size())
+        x2 = self.down1(x1)
+        # print('x2 : ', x2.size())
+        x3 = self.down2(x2)
+        # print('x3 : ', x3.size())
+        x4 = self.down3(x3)
+        # print('x4 : ', x4.size())
+        x5 = self.down4(x4)
+        # print('x5 : ', x5.size())
+        x = self.up1(x5, x4)
+        # print('x : ', x.size())
+        x = self.up2(x, x3)
+        # print('x : ', x.size())
+        x = self.up3(x, x2)
+        # print('x : ', x.size())
+        x = self.up4(x, x1)
+        # print('x : ', x.size())
+        sem = self.sem_out(x)
+        # print('sem : ', sem.size())
+        ins = self.ins_out(x)
+        # print('ins : ', ins.size())
+        spline = self.spline_out(x)
+        return sem, ins, spline
+
+if __name__=='__main__':
+    model = outlinear(64, 60 * 8 * 2)
+
+    a = np.empty((1, 64, 256, 256))
+    b = model.forward(torch.Tensor(a))
+
+    print(b.shape)
