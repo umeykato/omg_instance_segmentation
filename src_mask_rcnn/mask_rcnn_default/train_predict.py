65adff2747378c7a513e9f1bd8c52bddc9f016c2
DESKTOP-RMFRGL4\y_kato <y_kato@umelab.jp>
2018-12-12 14:37:54 +0900

    predictプログラム追加

---
+++ src_mask_rcnn/mask_rcnn_default/train_predict.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import os.path as osp
+import sys
+
+import chainer
+
+sys.path.append(osp.join(osp.dirname(__file__), '..'))
+import chainer_mask_rcnn as cmr
+
+here = osp.dirname(osp.abspath(__file__))  # NOQA
+sys.path.insert(0, osp.join(here, '..'))  # NOQA
+
+import train_common_predict
+import dataset
+
+
+def main():
+    args = train_common_predict.parse_args()
+
+    args.logs_dir = osp.join(here, 'logs')
+
+    # Dataset.
+    # args.dataset = 'coco'
+    # train_data = chainer.datasets.ConcatenatedDataset(
+    #     cmr.datasets.COCOInstanceSegmentationDataset('train'),
+    #     cmr.datasets.COCOInstanceSegmentationDataset('valminusminival'),
+    # )
+    
+    # test_data = cmr.datasets.COCOInstanceSegmentationDataset(
+    #     'minival',
+    #     use_crowd=True,
+    #     return_crowd=True,
+    #     return_area=True,
+    # )
+    # args.class_names = tuple(test_data.class_names.tolist())
+
+    args.dataset = 'original'
+    train_data = dataset.OomugiDataset()
+    test_data = dataset.OomugiDataset(test=True)
+    args.class_names = ['leaf']
+
+    # Model.
+    args.min_size = 800
+    args.max_size = 1333
+    args.anchor_scales = (2, 4, 8, 16, 32)
+    args.ratios = (0.11, 0.14, 0.2, 0.25, 0.33, 0.5, 1, 2, 3, 4, 5, 7, 9)
+
+    # Run training!.
+    train_common_predict.train(
+        args=args,
+        train_data=train_data,
+        test_data=test_data,
+        evaluator_type='coco',
+    )
+
+
+if __name__ == '__main__':
+    main()
