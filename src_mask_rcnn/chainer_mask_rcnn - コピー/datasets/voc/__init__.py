d6df2b841aa3c8585da620434d2675de0727ef3f
DESKTOP-RMFRGL4\y_kato <y_kato@umelab.jp>
2018-12-17 19:29:37 +0900

    no message

--- src_mask_rcnn/chainer_mask_rcnn - コピー/datasets/voc/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-from .sbd import SBDInstanceSeg
-from .sbd import SBDInstanceSegmentationDataset
-from .voc import VOC2012InstanceSeg
-from .voc import VOC2012InstanceSegmentationDataset
-from .voc import VOCInstanceSegBase
